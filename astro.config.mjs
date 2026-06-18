8ad16f8922d8ff4ab67a6dad3574ea882a86e7a0
Joey <joeysmccracken@gmail.com>
2026-06-18 15:48:27 +0100

    Added preact island for custom greeting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://josephmccracken.netlify.app/"
+  site: "https://josephmccracken.netlify.app/",
+  integrations: [preact()]
 });
